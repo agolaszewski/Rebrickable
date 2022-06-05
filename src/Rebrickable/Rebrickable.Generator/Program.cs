229d3c76dcbb7276999acedef72c52226fccb4a9
Andrzej Gołaszewski <agolaszewski>
2022-06-05 23:25:55 +0200

    generator project

---
+++ src/Rebrickable/Rebrickable.Generator/Program.cs
@@ -0,0 +1,26 @@
+﻿using NSwag;
+using NSwag.CodeGeneration.CSharp;
+
+var document = await OpenApiDocument.FromUrlAsync("https://rebrickable.com/api/v3/swagger/?format=openapi");
+var clientSettings = new CSharpClientGeneratorSettings
+{
+    
+    ClassName = "RebrickableApiClient",
+    CSharpGeneratorSettings =
+    {
+        Namespace = "Rebrickable.Api",
+    },
+    InjectHttpClient = true,
+    ExceptionClass = "RebrickableApiException",
+    ClientClassAccessModifier = "public",
+
+
+};
+
+var clientGenerator = new CSharpClientGenerator(document, clientSettings);
+var generatedClientFile = clientGenerator.GenerateFile();
+//generatedClientFile = generatedClientFile.Replace(" virtual ", " ");
+//generatedClientFile = generatedClientFile.Replace("protected ", "private ");
+
+Console.Write(generatedClientFile);
+
