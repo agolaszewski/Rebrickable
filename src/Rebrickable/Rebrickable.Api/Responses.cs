26ae74d7f00fccb50a1d0b351293a0eb9956777c
Andrzej Gołaszewski <agolaszewski>
2022-06-10 01:11:43 +0200

    LegoSetsListAsyncResponse

---
+++ src/Rebrickable/Rebrickable.Api/Responses.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+//https://json2csharp.com/
+namespace RebrickableApi
+{
+    public record Result(
+        [property: JsonProperty("set_num")] string SetNum,
+        [property: JsonProperty("name")] string Name,
+        [property: JsonProperty("year")] int Year,
+        [property: JsonProperty("theme_id")] int ThemeId,
+        [property: JsonProperty("num_parts")] int NumParts,
+        [property: JsonProperty("set_img_url")] string SetImgUrl,
+        [property: JsonProperty("set_url")] string SetUrl,
+        [property: JsonProperty("last_modified_dt")] DateTime LastModifiedDt
+    );
+
+    public record LegoSetsListAsyncResponse(
+        [property: JsonProperty("count")] int Count,
+        [property: JsonProperty("next")] string Next,
+        [property: JsonProperty("previous")] string Previous,
+        [property: JsonProperty("results")] IReadOnlyList<Result> Results
+    );
+}
--- src/Rebrickable/Rebrickable.Generator/Program.cs
+++ src/Rebrickable/Rebrickable.Generator/Program.cs
@@ -2,7 +2,10 @@
 using NSwag.CodeGeneration.CSharp;
 using NSwag.CodeGeneration.OperationNameGenerators;
 
-var document = await OpenApiDocument.FromUrlAsync("https://rebrickable.com/api/v3/swagger/?format=openapi");
+var apiUrl = args.Length > 0 ? args[0] : "https://rebrickable.com/api/v3/swagger/?format=openapi";
+var key = args.Length > 1 ? args[1] : "";
+
+var document = await OpenApiDocument.FromUrlAsync(apiUrl);
 var clientSettings = new CSharpClientGeneratorSettings
 {
     ClassName = "RebrickableClient",
@@ -14,6 +17,7 @@ var clientSettings = new CSharpClientGeneratorSettings
     ExceptionClass = "RebrickableApiException",
     OperationNameGenerator = new SingleClientFromOperationIdOperationNameGenerator(),
     ClientClassAccessModifier = "public sealed ",
+    GenerateOptionalParameters = true
 };
 
 var clientGenerator = new CSharpClientGenerator(document, clientSettings);
@@ -37,5 +41,64 @@ generatedClientFile = string.Join("", charList);
 generatedClientFile = generatedClientFile.Replace("private string BaseUrl", "private string _baseUrl");
 generatedClientFile = generatedClientFile.Replace("get { return BaseUrl; }", "get { return _baseUrl; }");
 generatedClientFile = generatedClientFile.Replace("set { BaseUrl = value; }", "set { _baseUrl = value; }");
+
+generatedClientFile = generatedClientFile.Replace("public async System.Threading.Tasks.Task ", "public async System.Threading.Tasks.Task<string> ");
+generatedClientFile = generatedClientFile.Replace("public System.Threading.Tasks.Task ", "public System.Threading.Tasks.Task<string> ");
+generatedClientFile = generatedClientFile.Replace("return;", "return await response.Content.ReadAsStringAsync();");
+generatedClientFile = generatedClientFile.Replace("using System = global::System;", "using System = global::System; using Newtonsoft.Json;");
+
+generatedClientFile = generatedClientFile
+    .Replace(
+        $"public async System.Threading.Tasks.Task<string> LegoSetsListAsync",
+        $"public async System.Threading.Tasks.Task<LegoSetsListAsyncResponse> LegoSetsListAsync");
+
+var legoSetsListAsyncIndex = generatedClientFile.LastIndexOf("LegoSetsListAsync");
+var search = generatedClientFile.Substring(legoSetsListAsyncIndex);
+var rest = ReplaceFirst(search, "return await response.Content.ReadAsStringAsync();", "return JsonConvert.DeserializeObject<LegoSetsListAsyncResponse>(await response.Content.ReadAsStringAsync());");
+generatedClientFile = generatedClientFile.Substring(0, legoSetsListAsyncIndex) + rest;
+
+string ReplaceFirst(string text, string search, string replace)
+{
+    int pos = text.IndexOf(search);
+    if (pos < 0)
+    {
+        return text;
+    }
+    return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+}
+
+
+
+//var lastClosingBracket = generatedClientFile.LastIndexOf('}');
+//generatedClientFile = generatedClientFile.Substring(0, lastClosingBracket) + "{responses} } " + generatedClientFile.Substring(lastClosingBracket);
+
+//var httpClient = new HttpClient();
+//httpClient.BaseAddress = new Uri("https://rebrickable.com");
+//httpClient.DefaultRequestHeaders.Add("Authorization", $"key {key}");
+
+//JsonClassGenerator generator = new JsonClassGenerator();
+//generator.UsePascalCase = true;
+//generator.SingleFile = true;
+//generator.AttributeUsage = JsonPropertyAttributeUsage.Always;
+//generator.OutputType = OutputTypes.ImmutableRecord;
+//string error = null;
+
+//var responses = string.Empty;
+
+//var legoSetsListAsyncResponse = await new RebrickableApi.RebrickableClient(httpClient).LegoSetsListAsync(page: 2);
+//var legoSetsListAsyncResponseRecords = generator.GenerateClasses(legoSetsListAsyncResponse, out error).ToString();
+//legoSetsListAsyncResponseRecords = legoSetsListAsyncResponseRecords.Replace("Root", "LegoSetsListAsync");
+
+//generatedClientFile = generatedClientFile
+//    .Replace(
+//        $"public async System.Threading.Tasks.Task<string> {nameof(RebrickableApi.RebrickableClient.LegoSetsListAsync)}",
+//        $"public async System.Threading.Tasks.Task<{nameof(RebrickableApi.RebrickableClient.LegoSetsListAsync)}> {nameof(RebrickableApi.RebrickableClient.LegoSetsListAsync)}");
+
+//generatedClientFile = generatedClientFile.Replace("return await response.Content.ReadAsStringAsync();", "return await response.Content.ReadAsStringAsync();");
+
+//responses += legoSetsListAsyncResponseRecords;
+//responses += System.Environment.NewLine;
+
+//generatedClientFile = generatedClientFile.Replace("{responses}", responses);
 
 Console.Write(generatedClientFile);
