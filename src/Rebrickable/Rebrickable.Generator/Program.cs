b029691bc5ff46c2967eb5f5607f93ba6c6b236d
Andrzej Gołaszewski <agolaszewski>
2022-06-14 20:52:49 +0200

    client with interface

--- src/Rebrickable/Rebrickable.Generator/Program.cs
+++ src/Rebrickable/Rebrickable.Generator/Program.cs
@@ -17,7 +17,8 @@ var clientSettings = new CSharpClientGeneratorSettings
     ExceptionClass = "RebrickableApiException",
     OperationNameGenerator = new SingleClientFromOperationIdOperationNameGenerator(),
     ClientClassAccessModifier = "public sealed ",
-    GenerateOptionalParameters = true
+    GenerateOptionalParameters = true,
+    GenerateClientInterfaces = true
 };
 
 var clientGenerator = new CSharpClientGenerator(document, clientSettings);
@@ -25,8 +26,7 @@ var generatedClientFile = clientGenerator.GenerateFile();
 generatedClientFile = generatedClientFile.Replace(" virtual ", " ");
 generatedClientFile = generatedClientFile.Replace("protected ", "private ");
 
-generatedClientFile = generatedClientFile.Replace("public async System.Threading.Tasks.Task ", "public async System.Threading.Tasks.Task<string> ");
-generatedClientFile = generatedClientFile.Replace("public System.Threading.Tasks.Task ", "public System.Threading.Tasks.Task<string> ");
+generatedClientFile = generatedClientFile.Replace(".Task ", ".Task<string> ");
 generatedClientFile = generatedClientFile.Replace("return;", "return await response_.Content.ReadAsStringAsync();");
 generatedClientFile = generatedClientFile.Replace("using System = global::System;", "using System = global::System; using Newtonsoft.Json;");
 
@@ -55,14 +55,15 @@ for (int index = 0; index < charList.Count;)
 }
 
 generatedClientFile = string.Join("", charList);
-//generatedClientFile = generatedClientFile.Replace("private string BaseUrl", "private string _baseUrl");
-//generatedClientFile = generatedClientFile.Replace("get { return BaseUrl; }", "get { return _baseUrl; }");
-//generatedClientFile = generatedClientFile.Replace("set { BaseUrl = value; }", "set { _baseUrl = value; }");
-
 generatedClientFile = generatedClientFile
     .Replace(
         $"public async System.Threading.Tasks.Task<string> LegoSetsListAsync",
         $"public async System.Threading.Tasks.Task<LegoSetsListAsyncResponse> LegoSetsListAsync");
+
+generatedClientFile = generatedClientFile
+    .Replace(
+        $"System.Threading.Tasks.Task<string> LegoSetsListAsync",
+        $"System.Threading.Tasks.Task<LegoSetsListAsyncResponse> LegoSetsListAsync");
 
 var legoSetsListAsyncIndex = generatedClientFile.LastIndexOf("LegoSetsListAsync");
 var search = generatedClientFile.Substring(legoSetsListAsyncIndex);
@@ -78,37 +79,5 @@ string ReplaceFirst(string text, string search, string replace)
     }
     return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
 }
-
-//var lastClosingBracket = generatedClientFile.LastIndexOf('}');
-//generatedClientFile = generatedClientFile.Substring(0, lastClosingBracket) + "{responses} } " + generatedClientFile.Substring(lastClosingBracket);
-
-//var httpClient = new HttpClient();
-//httpClient.BaseAddress = new Uri("https://rebrickable.com");
-//httpClient.DefaultRequestHeaders.Add("Authorization", $"key {key}");
-
-//JsonClassGenerator generator = new JsonClassGenerator();
-//generator.UsePascalCase = true;
-//generator.SingleFile = true;
-//generator.AttributeUsage = JsonPropertyAttributeUsage.Always;
-//generator.OutputType = OutputTypes.ImmutableRecord;
-//string error = null;
-
-//var responses = string.Empty;
-
-//var legoSetsListAsyncResponse = await new RebrickableApi.RebrickableClient(httpClient).LegoSetsListAsync(page: 2);
-//var legoSetsListAsyncResponseRecords = generator.GenerateClasses(legoSetsListAsyncResponse, out error).ToString();
-//legoSetsListAsyncResponseRecords = legoSetsListAsyncResponseRecords.Replace("Root", "LegoSetsListAsync");
-
-//generatedClientFile = generatedClientFile
-//    .Replace(
-//        $"public async System.Threading.Tasks.Task<string> {nameof(RebrickableApi.RebrickableClient.LegoSetsListAsync)}",
-//        $"public async System.Threading.Tasks.Task<{nameof(RebrickableApi.RebrickableClient.LegoSetsListAsync)}> {nameof(RebrickableApi.RebrickableClient.LegoSetsListAsync)}");
-
-//generatedClientFile = generatedClientFile.Replace("return await response.Content.ReadAsStringAsync();", "return await response.Content.ReadAsStringAsync();");
-
-//responses += legoSetsListAsyncResponseRecords;
-//responses += System.Environment.NewLine;
-
-//generatedClientFile = generatedClientFile.Replace("{responses}", responses);
 
 Console.Write(generatedClientFile);
