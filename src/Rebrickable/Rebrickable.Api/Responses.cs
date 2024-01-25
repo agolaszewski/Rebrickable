051b6faf7969e10696fe77a5a0765c14dad2143f
Andrzej Gołaszewski <andrusza1989@gmail.com>
2024-01-25 20:45:24 +0100

    fix LegoSetsReadAsync response

--- src/Rebrickable/Rebrickable.Api/Responses.cs
+++ src/Rebrickable/Rebrickable.Api/Responses.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
 //https://json2csharp.com/
@@ -38,14 +37,14 @@ namespace Rebrickable.Api
     }
 
     public record LegoSetsReadAsyncResponse(
-        [property: JsonPropertyName("set_num")] string SetNum,
-        [property: JsonPropertyName("name")] string Name,
-        [property: JsonPropertyName("year")] int Year,
-        [property: JsonPropertyName("theme_id")] int ThemeId,
-        [property: JsonPropertyName("num_parts")] int NumParts,
-        [property: JsonPropertyName("set_img_url")] string SetImgUrl,
-        [property: JsonPropertyName("set_url")] string SetUrl,
-        [property: JsonPropertyName("last_modified_dt")] DateTime LastModifiedDt
+        [property: JsonProperty("set_num")] string SetNum,
+        [property: JsonProperty("name")] string Name,
+        [property: JsonProperty("year")] int Year,
+        [property: JsonProperty("theme_id")] int ThemeId,
+        [property: JsonProperty("num_parts")] int NumParts,
+        [property: JsonProperty("set_img_url")] string SetImgUrl,
+        [property: JsonProperty("set_url")] string SetUrl,
+        [property: JsonProperty("last_modified_dt")] DateTime LastModifiedDt
     );
 
     public record LegoSetsMinifigsListAsyncResponse(
---
+++ src/Rebrickable/Rebrickable.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Rebrickable.Api;
+using System.Net.Http;
+
+namespace Rebrickable.Console
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+            .Build();
+
+            var handler = new HttpClientHandler();
+   
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(config["Rebrickable:Url"]);
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"key {config["Rebrickable:Key"]}");
+
+            try
+            {
+                var r = new RebrickableClient(httpClient);
+                var xd = await r.LegoSetsReadAsync("42083-1");
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e);
+                throw;
+            }
+            
+        }
+    }
+}
